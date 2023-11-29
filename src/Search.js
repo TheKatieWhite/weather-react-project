b23e1fc7a37c9b318b06f58add92d7ec22e08a7e
TheKatieWhite <132315102+TheKatieWhite@users.noreply.github.com>
2023-11-29 11:32:40 +0900

    added axios

--- src/Search.js
+++ src/Search.js
@@ -1,31 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 export default function Search() {
-  return (
-    <form id="enterCity">
-      <div className="container">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="text"
-              className="form-control entercity"
-              placeholder="Enter a city..."
-              autoComplete="off"
-              id="city-input"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <button
-              type="submit"
-              className="btn btn-primary"
-              id="button-search"
-            >
-              Search
-            </button>
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+    setReady(true);
+  }
+
+ 
+
+  if (ready) {
+   
+    return (
+      <form id="enterCity">
+        <div className="container">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="text"
+                className="form-control entercity"
+                placeholder="Enter a city..."
+                autoComplete="off"
+                id="city-input"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <button
+                type="submit"
+                className="btn btn-primary"
+                id="button-search"
+              >
+                Search
+              </button>
+              <p>Only here to test if working<h1>{Math.round(temperature)}</h1></p>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
-  );
+      </form>
+    );
+  }
+  else {
+let apiKey = "96771e971243152d6b8948878c26adde";
+
+let city = "Tokyo";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+axios.get(apiUrl).then(handleResponse);
+
+return "Loading...";
+
+  }
+
+
 }
